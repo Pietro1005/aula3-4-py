4c6a0739088c3bcbf2a1aa32d9a3bece88adf961
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2025-10-08 14:53:35 +0000

    fbhfh

---
+++ calc0.py
@@ -0,0 +1,15 @@
+print("-------calculadora--------")
+n1 = float(input("informe um numero: "))
+n2 = float(input("informe um numero: "))
+
+resultado = n1 + n2
+print("A soma é: ", resultado)
+
+resultado = n1 * n2
+print("a multiplicação é:", resultado)
+
+resultado = n1 - n2
+print ("a subtração é:", resultado)
+
+resultado = n1 / n2
+print("a divisão é:", resultado)
