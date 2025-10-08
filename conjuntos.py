f330d4a51f9184139258fc7a1b1962ec8dc1a087
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2025-10-08 15:55:00 +0000

    rhhf

---
+++ conjuntos.py
@@ -0,0 +1,20 @@
+#1. lista de compras
+compras = ['arroz', 'feijao', 'leite', 'pao']
+
+#Coordenadas geogrC!ficas
+coordenadas_brasilia = (15.7801, -47.9292)
+
+#3. cpfs de clientes
+cpfs = {'123.456.789-00', '987.654.321-00', '111.222.333-44'}
+
+#4. Notas dos alunos
+notas = {'João': 8.5, 'Maria': 9.0, 'Pedro': 7.0} 
+
+#5. Tarefas
+tarefas = ['Estudar para prova', 'Levar o cachorro pra passear', 'Lavar a roupa']
+
+#6. Meses do ano
+meses =  ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto')
+
+#7. primeiro nome de alunos
+alunos = {'João', 'Maria', 'Pedro', 'Ana'}
