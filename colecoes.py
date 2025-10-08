283d49f3e574b2f2a2b76fd930c249d0b1edd06e
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2025-10-08 16:09:39 +0000

    asf

---
+++ colecoes.py
@@ -0,0 +1,13 @@
+lista =[35, 23, 69, 25, 10, 2]
+
+menor = min(lista)
+maior = max (lista)
+elementos = len(lista)
+ordenada = sorted(lista)
+media = sum(lista) / len(lista)
+
+print(menor)
+print(maior)
+print(elementos)
+print (ordenada)
+print (media)
